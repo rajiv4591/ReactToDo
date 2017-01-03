bbc0edb1d16ee220e00992e976a67b016bae70c4
Rajiv Ratan Reddy Dwarampudi <Rajiv Ratan Reddy Dwarampudi>
2017-01-03 07:36:37 -0500

    added created and completed timestamps

--- app/tests/components/ToDoApp.test.jsx
+++ app/tests/components/ToDoApp.test.jsx
@@ -21,13 +21,17 @@ describe('ToDoApp', () => {
         todoApp.handleAddTodo(todoText);
 
         expect(todoApp.state.todos[0].text).toBe(todoText);
+
+        expect(todoApp.state.todos[0].createdAt).toBeA('number');
     });
 
     it('should toggle completed value when handletoggle called', () => {
         var todoData = {
             id: 11,
             text: 'test features',
-            completed: false
+            completed: false,
+            createdAt: 0,
+            completedAt: undefined
         };
 
         var todoApp = TestUtils.renderIntoDocument(<ToDoApp/>);
@@ -41,5 +45,31 @@ describe('ToDoApp', () => {
         todoApp.handleToggle(11);
 
         expect(todoApp.state.todos[0].completed).toBe(true);
+
+        expect(todoApp.state.todos[0].completedAt).toBeA('number');
+    });
+
+    it('should toggle todo from completed to incomplete', () => {
+        var todoData = {
+            id: 11,
+            text: 'test features',
+            completed: true,
+            createdAt: 0,
+            completedAt: 123
+        };
+
+        var todoApp = TestUtils.renderIntoDocument(<ToDoApp/>);
+
+        todoApp.setState(
+            {todos: [todoData]}
+        );
+
+        expect(todoApp.state.todos[0].completed).toBe(true);
+
+        todoApp.handleToggle(11);
+
+        expect(todoApp.state.todos[0].completed).toBe(false);
+
+        expect(todoApp.state.todos[0].completedAt).toNotExist();
     });
 });
